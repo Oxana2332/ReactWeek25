0959fa6b03499473256752b220c24406b5ddaaf4
Oxana2332 <far_north@ro.ru>
2022-11-07 18:03:57 +0300

    Pie Chart React Google Charts

---
+++ new/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { Chart } from 'react-google-charts';
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Html", 3],
+  ["CSS", 3],
+  ["Javascript", 7],
+  ["React", 6],
+];
+
+export const options = {
+  title: "Frontend study",
+};
+
+function App() {
+  return (
+    <div className="App">
+    <Chart
+    chartType="PieChart"
+    data={data}
+    options={options}
+    width={"100%"}
+    height={"400px"}
+    />
+    </div>
+  );
+}
+
+export default App;
